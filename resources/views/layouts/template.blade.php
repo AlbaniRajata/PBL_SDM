8207bf0725788c0935845ed9821e9e5cf5af0fa8
Nurhidayah <nurhid180701@gmail.com>
2024-11-22 21:01:54 +0700

    Modifikasi SideBar (Logo Polinema)

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,127 +1,121 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="{{ url('polinema.png') }}" type="image/png">
-  <title>{{ $title ?? 'SI-SDM' }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ url('polinema.png') }}" type="image/png">
+    <title>{{ $title ?? 'SI-SDM' }}</title>
 
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <style>
-      .brand-link {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-    }
-    .brand-image {
-        margin-bottom: 10px; /* Optional: add some spacing between the image and text */
-    }
-  </style>
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ url('plugins/fontawesome-free/css/all.min.css') }}">
-  <script src="https://kit.fontawesome.com/f2110b96b9.js" crossorigin="anonymous"></script>
-  <!-- DataTables -->
-  <link rel="stylesheet" href="{{ url('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ url('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="{{ url('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
-  <script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-  <script src="{{ url('plugins/jszip/jszip.min.js') }}"></script>
-  <script src="{{ url('plugins/pdfmake/pdfmake.min.js') }}"></script>
-  <script src="{{ url('plugins/pdfmake/vfs_fonts.js') }}"></script>
-  <script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-  <script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
-  <!-- SweetAlert2 -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <!-- Toastr -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ url('dist/css/adminlte.min.css') }}">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ url('plugins/fontawesome-free/css/all.min.css') }}">
+    <script src="https://kit.fontawesome.com/f2110b96b9.js" crossorigin="anonymous"></script>
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ url('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ url('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ url('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <!-- SweetAlert2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- Toastr -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ url('dist/css/adminlte.min.css') }}">
 
 </head>
+
 <body class="hold-transition sidebar-mini">
-<!-- Site wrapper -->
-<div class="wrapper">
-  <!-- Navbar -->
-  @include('layouts.header')
-  <!-- /.navbar -->
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        <!-- Navbar -->
+        @include('layouts.header')
+        <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="{{url('/')}}" class="brand-link">
-      <img src="{{url('polinema.png')}}" alt="AdminLTE Logo" class="brand-image img-cube" height="100px" style="opacity: 100">
-      <span class="brand-text font-weight-light" style="font-size: 20px;"><b>SI</b>-SDM</span>
-    </a>
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="{{ url('/') }}" class="brand-link"
+                style="display: flex; align-items: center; justify-content: center; text-align: center;">
+                <img src="{{ url('polinema.png') }}" alt="Logo" class="brand-image img-cube"
+                    style="margin-right: 10px; margin-left:-10px;">
+                <span class="brand-text font-weight-light"><b>SI</b>-SDM</span>
+            </a>
 
-    <!-- Sidebar -->
-    @include('layouts.sidebar')
-    <!-- /.sidebar -->
-  </aside>
+            <!-- Sidebar -->
+            @include('layouts.sidebar')
+            <!-- /.sidebar -->
+        </aside>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    @include('layouts.breadcrumb')
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            @include('layouts.breadcrumb')
 
-    <!-- Main content -->
-    <section class="content">
-    @yield('content')
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+            <!-- Main content -->
+            <section class="content">
+                @yield('content')
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
 
-  @include('layouts.footer')
-</div>
-<!-- ./wrapper -->
-{{-- push javascript if exist --}}
+        @include('layouts.footer')
+    </div>
+    <!-- ./wrapper -->
+    {{-- push javascript if exist --}}
 
-<!-- jQuery -->
-<script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ url('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- DataTables & Plugins -->
-<script src="{{ url('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-<script src="{{ url('plugins/jszip/jszip.min.js') }}"></script>
-<script src="{{ url('plugins/pdfmake/pdfmake.min.js') }}"></script>
-<script src="{{ url('plugins/pdfmake/vfs_fonts.js') }}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-<script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
-<!-- jquery-validation -->
-<script src="{{ url('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-<script src="{{ url('plugins/jquery-validation/additional-methods.min.js') }}"></script>
-<!-- SweetAlert2 -->
-<script src="{{ url('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ url('dist/js/adminlte.min.js') }}"></script>
-<script>
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-  })
-</script>
-@stack('js')
+    <!-- jQuery -->
+    <script src="{{ url('plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ url('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- DataTables & Plugins -->
+    <script src="{{ url('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <!-- jquery-validation -->
+    <script src="{{ url('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ url('plugins/jquery-validation/additional-methods.min.js') }}"></script>
+    <!-- SweetAlert2 -->
+    <script src="{{ url('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ url('dist/js/adminlte.min.js') }}"></script>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        })
+    </script>
+    @stack('js')
 </body>
+
 </html>
