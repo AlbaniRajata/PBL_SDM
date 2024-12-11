4cbcb7621eaa00c7fce23e20cf0ac3d80d86577a
Albani Rajata Malik <albeebro002@gmail.com>
2024-12-11 12:40:44 +0700

    Update API controller Kegiatan, Agenda, Kalender, Profil, Dashboard Content, Progress (Albi)

--- app/Http/Controllers/Api/KegiatanPimpinanController.php
+++ app/Http/Controllers/Api/KegiatanPimpinanController.php
@@ -7,6 +7,7 @@ use App\Models\JabatanKegiatanModel;
 use App\Models\KegiatanModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class KegiatanPimpinanController extends Controller
 {
@@ -78,6 +79,41 @@ class KegiatanPimpinanController extends Controller
             return response()->json([
                 'status' => false,
                 'message' => 'Gagal mengambil data jabatan: ' . $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function getKegiatanKalender()
+    {
+        try {
+            $kegiatan = KegiatanModel::select(
+                'id_kegiatan',
+                'nama_kegiatan',
+                'tanggal_acara'
+            )
+            ->whereNotNull('tanggal_acara')
+            ->orderBy('tanggal_acara')
+            ->get();
+            
+            $formattedKegiatan = $kegiatan->map(function ($item) {
+                $tanggalAcara = Carbon::parse($item->tanggal_acara)->format('d-m-Y');
+                
+                return [
+                    'id_kegiatan' => $item->id_kegiatan,
+                    'nama_kegiatan' => $item->nama_kegiatan,
+                    'tanggal_acara' => $tanggalAcara,
+                ];
+            });
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data kegiatan kalender berhasil diambil',
+                'data' => $formattedKegiatan
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Gagal mengambil data kegiatan kalender: ' . $e->getMessage()
             ], 500);
         }
     }
