d62b742171287a881ef5bf3806ad775fd9dcce47
Moch Fikri Setiawan <mixchannel307@gmail.com>
2024-12-17 16:55:41 +0700

    perbaikan show ajax admin,show ajax pimpinan, dan sidebar pimpinan

--- resources/views/admin/kegiatan/show_ajax.blade.php
+++ resources/views/admin/kegiatan/show_ajax.blade.php
@@ -89,41 +89,6 @@
                     @endforeach
                 </tbody>
             </table>
-            <!-- Formulir Upload File -->
-                <form action="{{ route('kegiatan.upload') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="form-group">
-                        <label for="file">Upload Surat Tugas:</label>
-                        <input type="file" class="form-control" id="file" name="file" required>
-
-                        <!-- Validasi error -->
-                        @error('file')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-
-                    <!-- Kirim ID kegiatan sebagai hidden input -->
-                    <input type="hidden" name="id_kegiatan" value="{{ $kegiatan->id_kegiatan }}">
-
-                    <!-- Riwayat File yang Telah Diunggah -->
-                    @if($dokumenTerbaru)
-                        <div class="mt-3 mb-3">
-                            <div class="list-group-item d-flex justify-content-between align-items-center" style="color: blue; text-decoration: none;">
-                                <span>{{ $dokumenTerbaru->nama_dokumen }}</span>
-                                </a>
-                            </div>
-                        </div>
-                    @else
-                        <div class="mt-4">
-                            <div class="alert alert-warning">Belum ada surat tugas yang diunggah.</div>
-                        </div>
-                    @endif
-
-                    <div class="d-flex justify-content-between">
-                        <button type="submit" class="btn btn-primary">Upload</button>
-                        <button type="button" class="btn btn-warning" data-dismiss="modal">Tutup</button>
-                    </div>
-                </form>
                 <div class="alert alert-info mt-3">
                     <h5><i class="icon fas fa-info"></i> Data Dokumen Anggota</h5>
                     Berikut adalah anggota yang terlibat dalam kegiatan ini
@@ -161,6 +126,41 @@
                             @endforelse
                         </tbody>
                 </table>
+                <!-- Formulir Upload File -->
+                <form action="{{ route('kegiatan.upload') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label for="file">Upload Surat Tugas:</label>
+                        <input type="file" class="form-control" id="file" name="file" required>
+
+                        <!-- Validasi error -->
+                        @error('file')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <!-- Kirim ID kegiatan sebagai hidden input -->
+                    <input type="hidden" name="id_kegiatan" value="{{ $kegiatan->id_kegiatan }}">
+
+                    <!-- Riwayat File yang Telah Diunggah -->
+                    @if($dokumenTerbaru)
+                        <div class="mt-3 mb-3">
+                            <div class="list-group-item d-flex justify-content-between align-items-center" style="color: blue; text-decoration: none;">
+                                <span>{{ $dokumenTerbaru->nama_dokumen }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    @else
+                        <div class="mt-4">
+                            <div class="alert alert-warning">Belum ada surat tugas yang diunggah.</div>
+                        </div>
+                    @endif
+
+                    <div class="d-flex justify-content-between">
+                        <button type="submit" class="btn btn-primary">Upload</button>
+                        <button type="button" class="btn btn-warning" data-dismiss="modal">Tutup</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
